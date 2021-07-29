55b58ecac0e9122befaec81990f87a7863f16f45
APotatoChip <cvetina.a.m@gmail.com>
2021-07-29 18:34:50 +0300

    login form validators

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -18,7 +19,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     SharedModule,
-    UserRoutingModule
+    UserRoutingModule,
+    FormsModule
   ],
   providers:[
     UserService
